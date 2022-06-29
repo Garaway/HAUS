607b5edcd383011144c1e37d39706a3744c39278
ninjavsrabbit <ninjavsrabbit@gmail.com>
2022-06-30 00:08:27 +0300

    fixed menu, added 3 section and bootstrap img

---
+++ script/head-show-hide-menu.js
@@ -0,0 +1,23 @@
+let lastScroll = 0;
+const defaultOffset = 200;
+const header = document.querySelector('.head__show');
+
+const scrollPosition = () => document.documentElement.scrollTop;
+const scrollHide = () => header.classList.contains('head__hide');
+
+window.addEventListener('scroll', () => {
+    if (scrollPosition() > lastScroll && !scrollHide() && scrollPosition() > defaultOffset) {
+        header.classList.add('head__hide');
+    } else if (scrollPosition() < lastScroll && scrollHide()) {
+        header.classList.remove('head__hide');
+    }
+
+    lastScroll = scrollPosition();
+
+})
+
+
+
+
+
+
